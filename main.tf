c7cf60439f0b5f6f003704b67c0063e92a04bf32
elala23 <lamyaaelaji2000@gmail.com>
2023-10-05 09:39:49 +0000

    partie3

--- main.tf
+++ main.tf
@@ -1,30 +1,30 @@
 
-resource "google_project_service" "cloudresource" {
+resource "google_project_service" "ressource_manager" {
     service = "cloudresourcemanager.googleapis.com"
-    project= "devops-tp2-399708"
+    
 }
 
 resource "google_project_service" "serviceusage" {
     service = "serviceusage.googleapis.com"
-    project= " devops-tp2-399708"
+    depends_on = [ google_project_service.ressource_manager ]
     
 }
 
 resource "google_project_service" "artifact" {
     service = "artifactregistry.googleapis.com"
-    project= "devops-tp2-399708"
+    depends_on = [ google_project_service.ressource_manager ]
    
 }
 
 resource "google_project_service" "sql" {
     service = "sqladmin.googleapis.com"
-    project= "devops-tp2-399708"
+    depends_on = [ google_project_service.ressource_manager ]
  
 }
 
 resource "google_project_service" "cloudbuild" {
     service = "cloudbuild.googleapis.com"
-    project= " devops-tp2-399708"
+    depends_on = [ google_project_service.ressource_manager ]
     
 }
 
@@ -35,7 +35,7 @@ resource "google_artifact_registry_repository" "my-repo" {
   description   = "repo Docker - wordpress"
   format        = "DOCKER"
 
-  depends_on = [ google_project_service.artifact ]
+  
 }
 
 resource "google_sql_user" "wordpress" {
@@ -47,5 +47,50 @@ resource "google_sql_user" "wordpress" {
 resource "google_sql_database" "wordpress" {
    name     = "wordpress"
    instance = "main-instance"
-   password = "ilovedevops"
+  
+}
+
+
+resource "google_cloud_run_service" "default" {
+name     = "serveur-wordpress"
+location = "us-central1"
+
+template {
+   spec {
+      containers {
+      image = "us-central1-docker.pkg.dev/devops-tp2-399708/website-tools/wordpress"
+      ports {
+        container_port=80
+      }
+      }
+   }
+
+   metadata {
+      annotations = {
+            "run.googleapis.com/cloudsql-instances" = "devops-tp2-399708:us-central1:main-instance"
+      }
+   }
+}
+
+traffic {
+   percent         = 100
+   latest_revision = true
+}
+}
+
+data "google_iam_policy" "noauth" {
+   binding {
+      role = "roles/run.invoker"
+      members = [
+         "allUsers",
+      ]
+   }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+   location    = google_cloud_run_service.default.location
+   project     = google_cloud_run_service.default.project
+   service     = google_cloud_run_service.default.name
+
+   policy_data = data.google_iam_policy.noauth.policy_data
 }
